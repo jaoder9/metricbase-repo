476dbc132686040bb114f8c7c44e6534f7beb152
Valentin Hobusch <valentinhobusch@gmail.com>
2018-01-05 11:32:09 +0100

    changed settings

--- metricbase/settings.py
+++ metricbase/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from . import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+from secrets import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
