31f13cb53d45c763874c07a800ecb5ce0fed4009
Valentin Hobusch <valentinhobusch@gmail.com>
2018-01-06 16:11:36 +0100

    some styling

--- metrics/urls.py
+++ metrics/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^new/$', views.newmetric, name='newmetric'),
     url(r'^metrics/(?P<pk>\d+)/$', views.showmetric, name='showmetric'),
+    url(r'', views.allmetrics, name='index'),
     url(r'^metrics/$', views.allmetrics, name='allmetrics'),
 
 ]
